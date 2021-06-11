5a1cba5d7c9789f1f7308b18ab4d48af90b8dd1a
Bhoomi_Patel <bhumi@itoneclick.com>
2021-06-11 17:42:02 +0530

    first push

---
+++ Example/Pods/Target Support Files/BhoomiFramework/BhoomiFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BhoomiFramework : NSObject
+@end
+@implementation PodsDummy_BhoomiFramework
+@end
---
+++ Example/Pods/Target Support Files/Pods-BhoomiFramework_Tests/Pods-BhoomiFramework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BhoomiFramework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BhoomiFramework_Tests
+@end
